d2b94d07bda5c3c1a206cdfcea4839dd316cfdec
Maziar Sojoudian <maziar@sojoudian.net>
2024-07-11 15:21:33 -0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+sudo mkdir /var/app/
+cd /var/app/
+sudo wget https://raw.githubusercontent.com/sojoudian/goWebServerReturnLocalhost/master/goWebServerReturnLocalhost -O /var/app/goWebServerReturnLocalhost
+sleep 5 && sudo chmod u+x /var/app/goWebServerReturnLocalhost
+sleep 5 && sudo /var/app/goWebServerReturnLocalhost
